e288e67a64a81a11aaa1dddc8eeac60c162b575d
mdavis9988 <58867354+mdavis9988@users.noreply.github.com>
2020-02-21 14:45:26 -0500

    step 5 complete

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import 'normalize.css';
 import './index.css';
 import api from './api';
 import shoppingList from './shopping-list';
-
+import store from './store'; 
 
 
 const main = function () {
-
-  api.createItem('pears')
-    .then(res => res.json())
-    .then((newItem) => {
-      return api.getItems();
-    })
+  api.getItems()
     .then(res => res.json())
     .then((items) => {
-      console.log(items);
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
     });
+  
 
   shoppingList.bindEventListeners();
   shoppingList.render();
