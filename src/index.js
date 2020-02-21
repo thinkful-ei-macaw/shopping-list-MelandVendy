30dca5332161d9959155f1b5a35e20517a712457
mdavis9988 <58867354+mdavis9988@users.noreply.github.com>
2020-02-21 11:28:30 -0500

    Step 1 test code completed

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import './index.css';
 
 import shoppingList from './shopping-list';
 
+
+
 const main = function () {
+  
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
+
 
 $(main);
