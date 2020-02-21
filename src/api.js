2b0b7c3352c089417f9d7afc996b57edf983f807
Vendy Prum <vendy@thinkful.com>
2020-02-21 09:42:36 -0800

    step 4 completed

--- src/api.js
+++ src/api.js
@@ -3,9 +3,23 @@ const BASE_URL = 'https://thinkful-list-api.herokuapp.com/melvendy';
 
 function getItems(){
   return fetch(`${BASE_URL}/items`);
-
-   
 }
+
+function createItem(name){
+  const newItem = JSON.stringify({
+    name
+  });
+
+  return fetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: { 
+      'Content-Type': 'application/json' 
+    },
+    body: newItem
+  });
+} 
+
 export default {
-  getItems
+  getItems,
+  createItem
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import $ from 'jquery';
-import api from './api';
+
 import 'normalize.css';
 import './index.css';
-
+import api from './api';
 import shoppingList from './shopping-list';
 
 
 
 const main = function () {
-  api.getItems()
-    .then(res => res.json())
-    .then(res => console.log(res));
 
+  api.createItem('pears')
+    .then(res => res.json())
+    .then((newItem) => {
+      return api.getItems();
+    })
+    .then(res => res.json())
+    .then((items) => {
+      console.log(items);
+    });
 
   shoppingList.bindEventListeners();
   shoppingList.render();
